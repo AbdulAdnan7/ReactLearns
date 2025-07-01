80470e61f2d6ef9df9cd93701c227176dc5189b9
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-01 19:27:50 +0530

    Learned how to add styles on live clicked nav link using NavLink

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import styles from './Navbar.module.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='bg-gray-600'>
+    <div className='bg-gray-600 py-4'>
         <ul className='flex gap-4 justify-center'>
             <li className='text-2xl'>
-            <Link to='/'>Home</Link>
+            <NavLink to='/' className={({isActive}) =>    isActive ? 'bg-orange-500 text-white px-3 py-1 rounded' : 'text-white px-3 py-1'}>Home</NavLink>
         </li> 
           <li className='text-2xl'>
-            <Link to='/header'>Header</Link>
+            <NavLink to='/header' className={({isActive}) =>    isActive ? 'bg-orange-500 text-white px-3 py-1 rounded' : 'text-white px-3 py-1'}>Header</NavLink>
           </li>
           <li className='text-2xl'>
-            <Link to='/about'>
+            <NavLink to='/about' className={({isActive}) =>    isActive ? 'bg-orange-500 text-white px-3 py-1 rounded' : 'text-white px-3 py-1'}>
             About
-            </Link>
+            </NavLink>
           </li>
         </ul>
        
